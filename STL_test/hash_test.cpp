6b0b000dce50066ff940ae20566bbbf7813086fe
shecannotsee <shecannotsee@163.com>
2022-10-16 23:51:31 +0800

    312

--- STL_test/hash_test.cpp
+++ STL_test/hash_test.cpp
@@ -3,11 +3,27 @@
 // hash_test using c++11 standard
 //
 #include <iostream>
+#include <string>// hash
 
 
 int main() {
   std::cout<<"start.\n";
 
+  /* Simple usage */ {
+    std::string str = "hello world";
+    std::hash<std::string> myStringHash;
+    size_t stringHashVal = myStringHash(str);
+    std::cout << "std::string hash val : " << stringHashVal << std::endl;
+
+    const char* a = "hello world";
+    std::hash<const char*> myCharHash;
+    size_t intHashVal = myCharHash(a);
+    std::cout << "char* hash val : " << intHashVal <<std::endl;
+  };
+
+  /* define user's hash */ {
+
+  };
 
 
   std::cout<<"end.\n";
