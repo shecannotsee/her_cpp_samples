47cd1e5904ecfded365d7d741f3fe1addf64a460
shecannotsee <shecannotsee@163.com>
2022-10-23 23:07:24 +0800

    1.添加mysql5.7专项测试

---
+++ mysql5.7_c_test/header.h
@@ -0,0 +1,31 @@
+//
+// Created by pc on 2022/10/23.
+//
+
+#ifndef BUFFER_DESIGN_TEST_MYSQL5_7_C_TEST_HEADER_H_
+#define BUFFER_DESIGN_TEST_MYSQL5_7_C_TEST_HEADER_H_
+
+#include <mysql.h>
+
+namespace method1 {
+void baseInit(MYSQL& mysql) {
+  if(mysql_init(&mysql) == NULL) {
+    printf("%d : error : %s\n", __LINE__, mysql_error(&mysql));
+    exit(1);
+  }
+  if (NULL == mysql_real_connect(&mysql,"127.0.0.1",
+                                 "root","123456",
+                                 "test_environment",0,
+                                 NULL,0)) {
+    printf("%d : error : %s\n", __LINE__, mysql_error(&mysql));
+    exit(1);
+  }
+}
+
+void main() {
+  MYSQL mysql;
+  baseInit(mysql);
+};
+};//namespace method1
+
+#endif //BUFFER_DESIGN_TEST_MYSQL5_7_C_TEST_HEADER_H_
---
+++ mysql5.7_c_test/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by pc on 2022/10/23.
+//
+#include <iostream>
+#include "header.h"
+
+int main() {
+  std::cout<<"mysql test start.\n";
+  method1::main();
+
+  std::cout<<"end.\n";
+  return 0;
+};
