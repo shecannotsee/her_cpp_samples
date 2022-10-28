7f3a1216169d915c3c49238607dfed85db32c2cd
shecannotsee <shecannotsee@163.com>
2022-10-28 17:34:13 +0800

    feat:添加了大小端测试框架

---
+++ byte_order_test/header.h
@@ -0,0 +1,14 @@
+//
+// Created by shecannotsee on 2022/10/28.
+//
+
+#ifndef BASE64_TEST_BYTE_ORDER_TEST_HEADER_H_
+#define BASE64_TEST_BYTE_ORDER_TEST_HEADER_H_
+
+namespace method1 {
+void  main() {
+
+}
+};// namespace method1
+
+#endif //BASE64_TEST_BYTE_ORDER_TEST_HEADER_H_
---
+++ byte_order_test/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by shecannotsee on 2022/10/28.
+//
+#include <iostream>
+#include "header.h"
+
+int main() {
+  std::cout<<".\n";
+  method1::main();
+  std::cout<<"./\n";
+  return 0;
+};
