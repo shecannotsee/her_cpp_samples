c4a91724e0571e5664fc7ccb995f2252e54df9a2
shecannotsee <shecannotsee@163.com>
2022-11-03 13:44:18 +0800

    feat:添加jwt-cpp的使用代码，代码举例展示了应该如何处理token

---
+++ jwt-cpp_test/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by shecannotsee on 2022/11/3.
+//
+#include <iostream>
+#include "header.h"
+
+int main() {
+  std::cout<<"jwt-cpp test start.\n";
+  method1::main();
+  std::cout<<"end.\n";
+  return 0;
+};
