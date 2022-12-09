8294f4ee84ae5da78725a134fbb23165bf690f29
shecannotsee <shecannotsee@163.com>
2022-12-09 17:30:43 +0800

    feat:添加获取linux系统信息的接口

--- linux_system_load_test/getCpu.h
+++ linux_system_load_test/getCpu.h
@@ -14,34 +14,35 @@
 
 namespace getCpu {
 
-double get_cpu_usage(int cpu_id)
-{
+double get_cpu_usage(int cpu_id) {
   // 打开/proc/stat文件
   std::ifstream stat_file("/proc/stat");
-  if (!stat_file.is_open())
-  {
+  if (!stat_file.is_open()) {
     std::cerr << "Failed to open /proc/stat" << std::endl;
     return 0.0;
   }
 
   // 读取文件中的内容
   std::string line;
-  while (std::getline(stat_file, line))
-  {
+  while (std::getline(stat_file, line)) {
     // 如果找到了指定的核心
-    if (line.find("cpu" + std::to_string(cpu_id) + " ") == 0)
-    {
+    if (line.find("cpu" + std::to_string(cpu_id) + " ") == 0) {
       // 解析该行内容
       std::istringstream iss(line);
       std::vector<unsigned long long> values(10);
-      for (int i = 0; i < 10; i++)
-      {
+      for (int i = 0; i < 10; i++) {
         iss >> values[i];
       }
 
       // 计算核心的使用情况
-      return (values[1] + values[2] + values[3]) /
-          static_cast<double>(values[1] + values[2] + values[3] + values[4]);
+      if (values[1] + values[2] + values[3] == 0)
+      {
+        return 0.0;
+      }
+      else {
+        return (values[1] + values[2] + values[3]) /
+            static_cast<double>(values[1] + values[2] + values[3] + values[4]);
+      }
     }
   }
 
