4491cca51ffbf11361f3985b1903d8a373e69904
shecannotsee <shecannotsee@163.com>
2022-12-23 14:39:27 +0800

    feat:在fltk中使用opengl编程

--- fltk1.3.7_test/main.cpp
+++ fltk1.3.7_test/main.cpp
@@ -5,10 +5,12 @@
 #include "method_1.h"
 #include "method_2.h"
 #include "method_3.h"
+#include "method_4.h"
 
 int main(int argc, char **argv) {
 //  method_1::main(argc, argv);
 //  method_2::main(argc, argv);
-  method_3::main();
+//  method_3::main();
+  method_4::main(argc, argv);
   return 0;
 }
