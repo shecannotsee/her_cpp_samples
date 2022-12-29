1e9491f64f6a878b9da75e7ca2594cb14f9e222a
shecannotsee <shecannotsee@163.com>
2022-12-29 13:32:13 +0800

    feat:添加openssl aes cbc 128接口并成功通过测试

--- openssl3.0.0_interface_test/main.cpp
+++ openssl3.0.0_interface_test/main.cpp
@@ -6,12 +6,14 @@
 #include "method_1.h"
 #include "method_2.h"
 #include "method_3.h"
+#include "interface.h"
 
 int main() {
   std::cout<<"openssl test start.\n";
   method_1::main();
 //  method_2::main();
-  method_3::main();
+//  method_3::main();
+  interface::main();
   std::cout<<"openssl test end.\n";
   return 0;
 };
