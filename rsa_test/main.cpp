68a3bf6825636f8d50a2d53d398c042b7b5340b3
shecannotsee <shecannotsee@163.com>
2023-01-10 17:14:34 +0800

    feat:添加一些其他测试

---
+++ rsa_test/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by  shecannotsee on 2023/1/10.
+//
+
+#include <iostream>
+
+#include "method_1.h"
+#include "extend_test.h"
+#include "bignumber_test.h"
+
+int main() {
+  std::cout<<"rsa_test start.\n";
+//  bignumber_test::main();
+//  extend_test::main();
+  method_1::main();
+
+  std::cout<<"rsa_test end.\n";
+  return 0;
+};
