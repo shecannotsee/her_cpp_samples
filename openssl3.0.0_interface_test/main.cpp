60d45a07fc8f18733985318f0f3263dddb9b7a22
shecannotsee <shecannotsee@163.com>
2023-01-10 19:13:18 +0800

    feat:添加openssl的rsa接口

--- openssl3.0.0_interface_test/main.cpp
+++ openssl3.0.0_interface_test/main.cpp
@@ -6,15 +6,16 @@
 #include "method_1.h"
 #include "method_2.h"
 #include "method_3.h"
+#include "method_4_rsa.h"
 #include "aes_cbc_128_interface.h"
-#include "rsa_method_1.h"
 
 int main() {
   std::cout<<"openssl test start.\n";
 //  method_1::main();
 //  method_2::main();
 //  method_3::main();
-  aes_cbc_128_interface::main();
+  method_4_rsa::main();
+//  aes_cbc_128_interface::main();
   std::cout<<"openssl test end.\n";
   return 0;
 };
