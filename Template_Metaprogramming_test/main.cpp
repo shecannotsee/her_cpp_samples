66dbd635b3397b9cda72e4696ecfec66b421d486
shecannotsee <shecannotsee@163.com>
2023-01-14 16:23:19 +0800

    feat:添加模版元编程的架构

--- Template_Metaprogramming_test/main.cpp
+++ Template_Metaprogramming_test/main.cpp
@@ -2,10 +2,11 @@
 // Created by  shecannotsee on 2023/1/14.
 //
 #include <iostream>
+#include "method_1.h"
 
 int main() {
   std::cout<<"Template Metaprogramming test start.\n";
-
+  method_1::main();
   std::cout<<"Template Metaprogramming test end.\n";
   return 0;
 };
---
+++ Template_Metaprogramming_test/method_1.h
@@ -0,0 +1,17 @@
+//
+// Created by  shecannotsee on 2023/1/14.
+//
+
+#ifndef TEMPLATE_METAPROGRAMMING_TEST_METHOD_1_H
+#define TEMPLATE_METAPROGRAMMING_TEST_METHOD_1_H
+
+namespace method_1 {
+
+void main() {
+
+
+};
+
+};// namespace method_1
+
+#endif //TEMPLATE_METAPROGRAMMING_TEST_METHOD_1_H
