9e74d48307b51413f00799130a39df5055b8a6a0
shecannotsee <shecannotsee@163.com>
2023-01-14 16:20:41 +0800

    feat:添加模版元编程的架构

---
+++ Template_Metaprogramming_test/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by  shecannotsee on 2023/1/14.
+//
+#include <iostream>
+
+int main() {
+  std::cout<<"Template Metaprogramming test start.\n";
+
+  std::cout<<"Template Metaprogramming test end.\n";
+  return 0;
+};
