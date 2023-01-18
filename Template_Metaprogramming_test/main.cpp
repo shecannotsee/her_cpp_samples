f75cd16e0893f984992e6ef5bad982b2568c041e
shecannotsee <shecannotsee@163.com>
2023-01-18 15:01:31 +0800

    feat:添加tuple里查找类型的模板元编程代码

--- Template_Metaprogramming_test/main.cpp
+++ Template_Metaprogramming_test/main.cpp
@@ -3,10 +3,12 @@
 //
 #include <iostream>
 #include "method_1.h"
+#include "method_2_make_tuple.h"
 
 int main() {
   std::cout<<"Template Metaprogramming test start.\n";
   method_1::main();
+  method_2_make_tuple::main();
   std::cout<<"Template Metaprogramming test end.\n";
   return 0;
 };
--- Template_Metaprogramming_test/method_2_make_tuple.h
+++ Template_Metaprogramming_test/method_2_make_tuple.h
@@ -5,19 +5,74 @@
 #ifndef TEMPLATE_METAPROGRAMMING_TEST_METHOD_2_MAKE_TUPLE_H
 #define TEMPLATE_METAPROGRAMMING_TEST_METHOD_2_MAKE_TUPLE_H
 
+#include <iostream>
 #include <string>
 #include <vector>
+#include <tuple>
 
 
 namespace method_2_make_tuple {
 
-bool contains(std::string name,std::vector<std::string> t) {
-  return false;
-}
+template<bool condition, typename THEN, typename ELSE>
+struct if_;
+
+template<typename THEN, typename ELSE>
+struct if_<true, THEN, ELSE> {
+  using type = THEN;
+};
+
+template<typename THEN, typename ELSE>
+struct if_<false, THEN, ELSE> {
+  using type = ELSE;
+};
+
+// function_1
+template<typename SEARCH, typename TUPLE, size_t start_from=0>
+struct contains_type :
+        if_< // IF
+          std::is_same<std::tuple_element_t<start_from, TUPLE>, SEARCH>::value,
+          // THEN
+          std::true_type,
+          // ELSE
+          typename if_ <
+            (start_from == std::tuple_size<TUPLE>::value -1 ),
+            // THEN
+            std::false_type,
+            // ELSE
+            contains_type<SEARCH, TUPLE, start_from +1 >
+          >::type
+        >::type
+{};
+
+// function_2
+template<typename SEARCH>
+struct contains_type<SEARCH, std::tuple<>, 0> : std::false_type {};
+
+bool contains(const std::string& search,const std::vector<std::string>& v,size_t start_from = 0 ) {
+  if (v[start_from] == search) {
+    return true;
+  }
+  else {
+    if ( start_from == v.size() - 1 ) {
+      return false;
+    }
+    else {
+      return contains(search, v, start_from+1);
+    }
+  }
+};
 
 void main() {
-  std::vector<std::string> t{"int","bool","float"};
-  contains("double",t);
+  std::vector<std::string> vec{"int","bool","float"};
+  std::cout << std::boolalpha << contains("bool",  vec) << "\n";
+  std::cout << std::boolalpha << contains("double",vec) << "\n";
+
+
+  std::tuple<int, bool, float> tuple;
+  std::cout << std::boolalpha<<contains_type<bool, decltype(tuple)>::value   << "\n";// function_1
+  std::cout << std::boolalpha<<contains_type<double, decltype(tuple)>::value << "\n";// function_1
+
+  std::cout<< contains_type<bool, std::tuple<> >::value<<"\n";// function_2
 };
 
 };// namespace method_2_make_tuple
