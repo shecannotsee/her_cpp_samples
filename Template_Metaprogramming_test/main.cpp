bb581a6f2d6bf52ca54c8075aab0152b3f679d94
shecannotsee <shecannotsee@163.com>
2023-01-20 18:08:07 +0800

    feat:print tuple

--- Template_Metaprogramming_test/main.cpp
+++ Template_Metaprogramming_test/main.cpp
@@ -4,11 +4,13 @@
 #include <iostream>
 #include "method_1.h"
 #include "method_2_make_tuple.h"
+#include "method_3.h"
 
 int main() {
   std::cout<<"Template Metaprogramming test start.\n";
   method_1::main();
   method_2_make_tuple::main();
+  method_3::main();
   std::cout<<"Template Metaprogramming test end.\n";
   return 0;
 };
---
+++ Template_Metaprogramming_test/method_3.h
@@ -0,0 +1,42 @@
+//
+// Created by  shecannotsee on 2023/1/18.
+//
+
+#ifndef TEMPLATE_METAPROGRAMMING_TEST_METHOD_3_H
+#define TEMPLATE_METAPROGRAMMING_TEST_METHOD_3_H
+
+#include <iostream>
+#include <type_traits>
+#include <tuple>
+
+namespace method_3 {
+
+void printN() {
+  std::cout<<"\n";
+};
+
+// printN递归单参数出口,需要放在多参数同名函数前,以便于编译器来进行实例化的匹配
+template<typename LAST>
+void printN(LAST t) {
+  std::cout << t << "\n";
+};
+
+template<typename T0, typename ... T1toN>
+void printN(T0 t, T1toN... rest) {
+  std::cout << t << ",";
+  printN(rest...);
+};
+
+void main() {
+  std::cout << std::boolalpha;
+  printN(9, "shecannotsee", 3.2, false);
+  printN();
+  printN("hello", "world");
+
+  auto tuple = std::make_tuple(9, "shecannotsee", 3.2, false);
+  print_tuple(tuple);
+};
+
+};// namespace method_3
+
+#endif //TEMPLATE_METAPROGRAMMING_TEST_METHOD_3_H
