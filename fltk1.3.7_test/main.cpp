389d90fd588276702b11760355bc9529e8b9b84e
shecannotsee <shecannotsee@163.coom>
2023-02-02 17:17:39 +0800

    docs:添加了部分方法的注释和构建问题

--- fltk1.3.7_test/main.cpp
+++ fltk1.3.7_test/main.cpp
@@ -8,9 +8,9 @@
 #include "method_4.h"
 
 int main(int argc, char **argv) {
-//  method_1::main(argc, argv);
+  method_1::main(argc, argv);
 //  method_2::main(argc, argv);
-  method_3::main();
-//  method_4::main(argc, argv);
+//  method_3::main();
+//  method_4::main(argc, argv);// 使用了opengl,需要添加对应的支持库
   return 0;
 }
