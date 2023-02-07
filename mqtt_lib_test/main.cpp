dbbe037978df4c48efb536fa962ef5b624200887
shecannotsee <shecannotsee@163.com>
2023-02-07 16:24:10 +0800

    feat:在测试时候先订阅再发布

--- mqtt_lib_test/main.cpp
+++ mqtt_lib_test/main.cpp
@@ -3,13 +3,19 @@
 //
 
 #include <iostream>
+#include <future>
 #include "method_1_base.h"
 #include "method_2_sub.h"
 
 int main() {
   std::cout<<"mqtt lib test.\n";
-  method_1_base::main();
+//  method_1_base::main();
 //  method_2_sub::main();
+  auto sub_future = std::async(method_2_sub::main);
+  auto pub_future = std::async(method_1_base::main);
+  sub_future.wait();
+  pub_future.get();
+
   std::cout<<"mqtt lib test.\n";
   return 0;
 };
