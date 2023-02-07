5712e4ca633c72782e7a789b539416c92f4c87d4
shecannotsee <shecannotsee@163.com>
2023-02-07 15:57:22 +0800

    feat:添加方法二

--- mqtt_lib_test/main.cpp
+++ mqtt_lib_test/main.cpp
@@ -4,10 +4,12 @@
 
 #include <iostream>
 #include "method_1_base.h"
+#include "method_2_sub.h"
 
 int main() {
   std::cout<<"mqtt lib test.\n";
   method_1_base::main();
+//  method_2_sub::main();
   std::cout<<"mqtt lib test.\n";
   return 0;
 };
