9996ab50d6202e8a6b7e45856573fdd4257b8702
shecannotsee <shecannotsee@163.com>
2023-02-07 16:23:09 +0800

    feat:添加异步订阅完整代码

--- mqtt_lib_test/method_2_sub.h
+++ mqtt_lib_test/method_2_sub.h
@@ -16,18 +16,60 @@ using ::std::endl;
 // 使用方法1中的结果处理函数
 using ::method_1_base::processing_results;
 
+volatile MQTTClient_deliveryToken deliveredtoken;
+
+void delivered(void *context, MQTTClient_deliveryToken dt) {
+  printf("Message with token value [%d] delivery confirmed\n", dt);
+  deliveredtoken = dt;
+}
+
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+  printf("Message arrived\n");
+  printf("     topic: [%s]\n", topicName);
+  printf("   message: [%.*s]\n", message->payloadlen, (char*)message->payload);
+  MQTTClient_freeMessage(&message);
+  MQTTClient_free(topicName);
+  return 1;
+}
+
+void connlost(void *context, char *cause) {
+  printf("\nConnection lost\n");
+  printf("     cause: %s\n", cause);
+}
+
+
 void main() {
 //////////////////////////////////////mqtt异步订阅///////////////////////////////////////////////////////////////////////
   string address = "tcp://localhost:1883";
+  char msg[] = "Hello World!";
+  string topic_name = "MQTT Examples";
   string client_id = "ExampleClientSub";
 
   MQTTClient client;
-  MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+  MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;/* init */{
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+  };
   int return_code = 0;
 
   return_code = MQTTClient_create(&client, address.c_str(), client_id.c_str(), MQTTCLIENT_PERSISTENCE_NONE, NULL); {
     processing_results(return_code, "MQTTClient_create");
   }
+  return_code = MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered); {
+    processing_results(return_code, "MQTTClient_setCallbacks");
+  };
+  return_code = MQTTClient_connect(client, &conn_opts); {
+    processing_results(return_code, "MQTTClient_connect");
+  };
+  /* log */ {
+    cout<<"Subscribing to topic ["<<topic_name<<"]"<<endl;
+    cout<<"for client ["<<client_id<<"] using QoS["<<method_1_base::Message_pass_at_least_once<<"]"<<endl;
+    cout<<"Press Q<Enter> to quit\n\n";
+  };
+  return_code = MQTTClient_subscribe(client, topic_name.c_str(), method_1_base::Message_pass_at_least_once); {
+    processing_results(return_code, "MQTTClient_subscribe");
+  };
+
 
 
 
