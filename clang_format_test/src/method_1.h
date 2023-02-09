055597f30c81e91471af4e8c25273f85b6ce8ae0
shecannotsee <shecannotsee@163.com>
2023-02-09 11:49:21 +0800

    feat:构建clang-format测试项目并添加一些基础代码

--- clang_format_test/src/method_1.h
+++ clang_format_test/src/method_1.h
@@ -5,7 +5,7 @@
 #ifndef CLANG_FORMAT_TEST_CLANG_FORMAT_TEST_SRC_METHOD_1_H_
 #define CLANG_FORMAT_TEST_CLANG_FORMAT_TEST_SRC_METHOD_1_H_
 
-#include <test_class.h>
+#include <test_class/test_class.h>
 
 namespace method_1 {
 
