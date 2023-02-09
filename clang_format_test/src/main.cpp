f7a4977ee319b2c78ddc270c4a2926895d895175
shecannotsee <shecannotsee@163.com>
2023-02-09 10:52:26 +0800

    feat:构建clang-format测试项目并添加一些基础代码

--- clang_format_test/src/main.cpp
+++ clang_format_test/src/main.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+#include <method_1.h>
 
 int main() {
   std::cout<<"test start.\n";
---
+++ clang_format_test/src/method_1.h
@@ -0,0 +1,20 @@
+//
+// Created by shecannotsee on 2023/2/9.
+//
+
+#ifndef CLANG_FORMAT_TEST_CLANG_FORMAT_TEST_SRC_METHOD_1_H_
+#define CLANG_FORMAT_TEST_CLANG_FORMAT_TEST_SRC_METHOD_1_H_
+
+#include <test_class.h>
+
+namespace method_1 {
+
+void main() {
+  test_class tool(7);
+  tool.set_data(6);
+  tool.get_data();
+};
+
+};
+
+#endif //CLANG_FORMAT_TEST_CLANG_FORMAT_TEST_SRC_METHOD_1_H_
