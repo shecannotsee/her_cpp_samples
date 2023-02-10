ca3518c4c2daa9727bd0f9c3b1508652899ffa37
shecannotsee <shecannotsee@163.com>
2023-02-10 15:17:20 +0800

    feat:实例搭建

---
+++ mqtt_lib_test/method_6_interface.h
@@ -0,0 +1,29 @@
+//
+// Created by shecannotsee on 2023/2/10.
+//
+
+#ifndef MQTT_LIB_TEST_MQTT_LIB_TEST_METHOD_6_INTERFACE_H_
+#define MQTT_LIB_TEST_MQTT_LIB_TEST_METHOD_6_INTERFACE_H_
+
+#include <string>
+#include <MQTTAsync.h>
+
+namespace method_6_interface {
+
+class MqttService {
+ private:
+ public:
+  MqttService() = delete;
+  explicit MqttService(std::string server_url)
+ public:
+  void sub
+  void send_message
+};
+
+void main() {
+
+};
+
+};// namespace method_6_interface {
+
+#endif //MQTT_LIB_TEST_MQTT_LIB_TEST_METHOD_6_INTERFACE_H_
