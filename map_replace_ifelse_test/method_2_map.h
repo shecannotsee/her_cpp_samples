4380948444e4b244afd720eb597e7a7b72d3e0a7
shecannotsee <shecannotsee@163.com>
2023-02-10 14:41:23 +0800

    feat:添加一种实现,试图模板化

--- map_replace_ifelse_test/method_2_map.h
+++ map_replace_ifelse_test/method_2_map.h
@@ -5,31 +5,48 @@
 #ifndef MAP_REPLACE_IFELSE_TEST_METHOD_2_MAP_H
 #define MAP_REPLACE_IFELSE_TEST_METHOD_2_MAP_H
 
+#include <iostream>
 #include <string>
 #include <unordered_map>
 #include <functional>
-#include <type_traits>
-
 namespace method_2_map {
 
-enum class if_branch : unsigned int {
-  b_1 = 1,
-  b_2,
-  b_3,
-  b_4,
-  b_5
+template<typename branch_name>
+class Function_map {
+ private:
+  std::unordered_map<branch_name, std::function<void(void*)>> functions_;
+ public:
+  Function_map() {
+    branch_name branch_1;
+    functions_.insert(std::make_pair(branch_1,std::bind(&FunctionMap::case_1, this, std::placeholders::_1)));
+    branch_name branch_2;
+    functions_.insert(std::make_pair(branch_2,std::bind(&FunctionMap::case_2, this, std::placeholders::_1)));
+    branch_name branch_3;
+    functions_.insert(std::make_pair(branch_3,std::bind(&FunctionMap::case_3, this, std::placeholders::_1)));
+  };
+ public:
+  void find(branch_name input_branch, void* params) {
+    auto it = functions_.find(input_branch);
+    if (it != functions_.end()) {
+      it->second(params);
+    } else {
+      std::cout << "Unknown branch" << std::endl;
+    }
+  };
+ private:
+  void case_1(void* params) {
+    std::cout<<static_cast<int>(*params);
+  };
+  void case_2(void* params) {
+
+  };
+  void case_3(void* params) {
+
+  };
+
 };
 
 void main() {
-  std::unordered_map<if_branch, std::string> hashMapError;
-  hashMapError.insert(std::make_pair(if_branch::b_1, "function_1"));
-
-  std::unordered_map<unsigned int, std::string> hash_;
-  hash_.insert(std::make_pair(static_cast<unsigned int>(if_branch::b_1), "function_1"));
-
-  std::unordered_map<std::underlying_type<if_branch>::type , std::string> hashMap;
-  hashMap.insert(std::make_pair(static_cast<std::underlying_type<if_branch>::type>(if_branch::b_1),"function"));
-
 
 };
 
