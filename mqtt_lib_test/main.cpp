59e983179891e0acb7b2a2d55cc98a8bc0855a24
shecannotsee <shecannotsee@163.com>
2023-02-17 13:46:04 +0800

    feat:添加cpp多条件发布接口编写

--- mqtt_lib_test/main.cpp
+++ mqtt_lib_test/main.cpp
@@ -4,32 +4,32 @@
 
 #include <iostream>
 #include <future>
-//#include "method_1_publish_sync.h"
-//#include "method_2_sub_async.h"
-//#include "method_3_publish_async.h"
-//#include "method_4_async_pub.h"
-//#include "method_5_async_sub.h"
-//#include "method_6_interface.h"
-//#include "method_7_async_ssl_pub.h"
-//#include "method_8_async_ssl_sub.h"
-//#include "method_9_cpp_sub.h"
-//#include "method_10_cpp_ssl_pub.h"
+#include "method_1_publish_sync.h"
+#include "method_2_sub_async.h"
+#include "method_3_publish_async.h"
+#include "method_4_async_pub.h"
+#include "method_5_async_sub.h"
+#include "method_6_interface.h"
+//#include "method_7_async_ssl_pub.h"// example to read
+//#include "method_8_async_ssl_sub.h"// example to read
+#include "method_9_cpp_sub.h"
+#include "method_10_cpp_ssl_pub.h"
 #include "method_11_cpp_pub.h"
 
 int main() {
   std::cout<<"mqtt lib test.\n";
-//  auto sub_future       = std::async(std::launch::async, method_2_sub_async::main);
-//  auto pub_sync_future  = std::async(std::launch::async, method_1_publish_sync::main);
-//  auto pub_async_future = std::async(std::launch::async, method_3_publish_async::main);
-//
-//  pub_sync_future.wait();
-//  pub_async_future.wait();
-//  sub_future.wait();
-//  method_3_publish_async::main();
-//  method_4_async_pub::main();
-//  method_5_async_sub::main();
-//  method_9_cpp_sub::main();
-//  method_10_cpp_ssl_pub::main();
+  auto sub_future       = std::async(std::launch::async, method_2_sub::main);
+  auto pub_sync_future  = std::async(std::launch::async, method_1_publish_sync::main);
+  auto pub_async_future = std::async(std::launch::async, method_3_publish_async::main);
+
+  pub_sync_future.wait();
+  pub_async_future.wait();
+  sub_future.wait();
+  method_3_publish_async::main();
+  method_4_async_pub::main();
+  method_5_async_sub::main();
+  method_9_cpp_sub::main();
+  method_10_cpp_ssl_pub::main();
   method_11_cpp_pub::main();
 
   std::cout<<"mqtt lib test.\n";
