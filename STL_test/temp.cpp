c9e3ea171fade383d25cc9d5dd8be92eee8d1217
shecannotsee <shecannotsee@163.com>
2023-03-02 10:02:01 +0800

    feat:添加一个demo

---
+++ STL_test/temp.cpp
@@ -0,0 +1,17 @@
+//
+// Created by shecannotsee on 2023/3/2.
+//
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+int main() {
+  char s[] = {'1','2','3','0','0','4','5'};
+  std::cout<<strlen(s)<<"\n";
+  printf("[%s]\n",s);
+
+
+  std::cout<<s;
+  std::cout<<"|||\n";
+  return 0;
+};
