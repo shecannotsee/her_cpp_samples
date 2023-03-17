c59783ed00d06b61f91315fa2bf59debdec7108e
shecannotsee <shecannotsee@163.com>
2023-03-17 15:56:48 +0800

    feat:添加gmock代码

--- googletest_test/main.cpp
+++ googletest_test/main.cpp
@@ -3,11 +3,13 @@
 //
 #include <iostream>
 #include "head.h"
+#include "m2_gmock_test.h"
 
-int main() {
+int main(int argc,char* argv[]) {
   std::cout<<"googletest test start.\n";
-  method1::main_r_void();
+//  method1::main_r_void();
 //  method1::main_r_int();
+  m2_gmock_test::main(argc,argv);
   std::cout<<"googletest test end.\n";
   return 0;
 };
