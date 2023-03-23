6a12e2413960ca479f49e7b7f53621d52c93e251
shecannotsee <shecannotsee@163.com>
2023-03-23 14:03:00 +0800

    feat:jsoncpp测试demo添加

---
+++ jsoncpp_test/m1_basic_test.h
@@ -0,0 +1,25 @@
+//
+// Created by shecannotsee on 2023/3/21.
+//
+
+#ifndef GOOGLETEST_TEST_JSONCPP_TEST_M1_BASIC_TEST_H_
+#define GOOGLETEST_TEST_JSONCPP_TEST_M1_BASIC_TEST_H_
+
+#include <json/json.h>
+
+namespace m1_basic_test {
+
+int main() {
+  Json::Value root;
+  JSONCPP_STRING errs;
+  Json::CharReaderBuilder build;
+
+
+
+
+  return 0;
+};
+
+};// namespace m1_basic_test
+
+#endif //GOOGLETEST_TEST_JSONCPP_TEST_M1_BASIC_TEST_H_
