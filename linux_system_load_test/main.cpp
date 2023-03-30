da936efedf7da5bd9cb4bf13e0ce6c868bd9a2f7
shecannotsee <shecannotsee@163.com>
2023-03-30 10:31:03 +0800

    feat:添加程序入参和系统环境获取

--- linux_system_load_test/main.cpp
+++ linux_system_load_test/main.cpp
@@ -6,7 +6,14 @@
 //#include "getCpu.h"
 #include "get_process_cpu_memory.h"
 
-int main() {
+int main (int argc,char* argv[],char* env[]) {
+  for (int i = 0; i < argc; i++) {
+    std::cout<<argv[i]<<"\n";
+  }
+
+  for (int i = 0; env[i] != nullptr; i++) {
+    //std::cout << env[i] << std::endl;
+  }
 //  getMemory::main();
 //  getCpu::main();
   get_process_cpu_memory::main();
