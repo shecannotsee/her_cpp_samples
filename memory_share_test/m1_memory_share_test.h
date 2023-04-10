cc225ee038353ffdbe98d088f5e786f37a4a70d0
shecannotsee <shecannotsee@163.com>
2023-04-10 15:26:54 +0800

    feat:共享内存demo

---
+++ memory_share_test/m1_memory_share_test.h
@@ -0,0 +1,76 @@
+//
+// Created by shecannotsee on 2023/4/10.
+//
+
+#ifndef T_M_TEST_MEMORY_SHARE_TEST_M1_MEMORY_SHARE_TEST_H_
+#define T_M_TEST_MEMORY_SHARE_TEST_M1_MEMORY_SHARE_TEST_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <string.h>
+
+#define SHM_NAME "/my_shared_memory"
+#define SHM_SIZE 4096
+
+namespace m1_memory_share_test {
+
+int main() {
+  int fd;
+  char *addr;
+  char buf[SHM_SIZE];
+
+  // 创建共享内存区域
+  fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+  if (fd == -1) {
+    perror("shm_open");
+    exit(1);
+  }
+
+  // 设置共享内存区域大小
+  if (ftruncate(fd, SHM_SIZE) == -1) {
+    perror("ftruncate");
+    exit(1);
+  }
+
+  // 映射共享内存到进程地址空间
+  addr = static_cast<char*>(mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+  if (addr == MAP_FAILED) {
+    perror("mmap");
+    exit(1);
+  }
+
+  // 向共享内存写入数据
+  sprintf(buf, "Hello, shared memory!");
+  memcpy(addr, buf, strlen(buf) + 1);
+
+  // 从共享内存读取数据并输出
+  printf("%s\n", addr);
+
+  // 解除共享内存映射
+  if (munmap(addr, SHM_SIZE) == -1) {
+    perror("munmap");
+    exit(1);
+  }
+
+  // 关闭共享内存文件描述符
+  if (close(fd) == -1) {
+    perror("close");
+    exit(1);
+  }
+
+  // 删除共享内存对象
+  if (shm_unlink(SHM_NAME) == -1) {
+    perror("shm_unlink");
+    exit(1);
+  }
+
+
+  return 0;
+};
+
+};// namespace m1_memory_share_test
+
+#endif //T_M_TEST_MEMORY_SHARE_TEST_M1_MEMORY_SHARE_TEST_H_
---
+++ memory_share_test/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by shecannotsee on 2023/4/10.
+//
+
+#include <iostream>
+
+#include "m1_memory_share_test.h"
+
+int main() {
+  std::cout << "memory shared test start.\n";
+  m1_memory_share_test::main();
+
+  std::cout << "memory shared test end.\n";
+  return 0;
+};
