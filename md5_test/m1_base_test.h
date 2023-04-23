cf03c631e721c8530e468b12262f4e953ff231f1
shecannotsee <shecannotsee@163.com>
2023-04-23 11:59:36 +0800

    [feat]添加md5测试框架

---
+++ md5_test/m1_base_test.h
@@ -0,0 +1,17 @@
+//
+// Created by shecannotsee on 23-4-23.
+//
+
+#ifndef CMAKE_TEST_MD5_TEST_M1_BASE_TEST_H_
+#define CMAKE_TEST_MD5_TEST_M1_BASE_TEST_H_
+
+namespace m1_base_test {
+
+void main() {
+
+
+};
+
+};// namespace m1_base_test
+
+#endif //CMAKE_TEST_MD5_TEST_M1_BASE_TEST_H_
--- md5_test/main.cpp
+++ md5_test/main.cpp
@@ -1,3 +1,15 @@
 //
 // Created by shecannotsee on 23-4-23.
 //
+#include <iostream>
+#include "m1_base_test.h"
+
+int main() {
+  std::cout << "md5 test start.\n";
+
+  m1_base_test::main();
+
+
+  std::cout << "md5 test done.\n";
+  return 0;
+};
