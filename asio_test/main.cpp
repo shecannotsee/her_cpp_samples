41177c358943ea9332b5ff27df95ef3b01036f59
shecannotsee <shecannotsee@163.com>
2023-04-25 16:37:34 +0800

    [feat]添加tcp server demo，但是无法正常运行，详见todo

--- asio_test/main.cpp
+++ asio_test/main.cpp
@@ -4,13 +4,14 @@
 #include <iostream>
 #include "m1_sync_timer_test.h"
 #include "m2_async_timer_test.h"
+#include "m3_sync_tcp_demo.h"
 
-int main() {
+int main(int argc,char* argv[]) {
   std::cout << "asio test start.\n";
 
-  m1_sync_timer_test::main();
-  m2_async_timer_test::main();
-
+//  m1_sync_timer_test::main();
+//  m2_async_timer_test::main();
+  m3_sync_tcp_demo::main(argc,argv);
 
   std::cout << "asio test done.\n";
   return 0;
