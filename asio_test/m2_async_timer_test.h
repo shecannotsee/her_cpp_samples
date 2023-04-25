c41518585782cc9c1d4a8c709863aa4f53e1c79a
shecannotsee <shecannotsee@163.com>
2023-04-25 11:38:49 +0800

    [feat]添加异步定时器的架构

---
+++ asio_test/m2_async_timer_test.h
@@ -0,0 +1,22 @@
+//
+// Created by shecannotsee on 23-4-25.
+//
+
+#ifndef ASIO_TEST_ASIO_TEST_M2_ASYNC_TIMER_TEST_H_
+#define ASIO_TEST_ASIO_TEST_M2_ASYNC_TIMER_TEST_H_
+
+
+
+namespace m2_async_timer_test {
+
+void print(const asio::error_code& /*e*/) {
+  std::cout << "Hello, world!" << std::endl;
+}
+
+void main() {
+
+};
+
+};
+
+#endif //ASIO_TEST_ASIO_TEST_M2_ASYNC_TIMER_TEST_H_
