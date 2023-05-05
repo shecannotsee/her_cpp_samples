616cc7ed79dbbf12e8265017a8ed782804c29a37
shecannotsee <shecannotsee@163.com>
2023-05-05 17:58:55 +0800

    [feat]添加tmp demo

--- t_m_test/main.cpp
+++ t_m_test/main.cpp
@@ -5,11 +5,12 @@
 #include <iostream>
 
 #include "m1_base.hpp"
+#include "m2_traits.h"
 
 int main() {
   std::cout << "template meta program test start.\n";
   m1_base::main();
-
+  m2_traits::main();
   std::cout << "template meta program test end.\n";
   return 0;
 };
