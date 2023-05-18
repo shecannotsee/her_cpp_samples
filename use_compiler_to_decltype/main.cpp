264298b88822c951ea408ec67fb491c1fdc95a06
shecannotsee <shecannotsee@163.com>
2023-05-18 11:53:42 +0800

    [feat]通过编译器的报错来查看自动推导的类型

--- use_compiler_to_decltype/main.cpp
+++ use_compiler_to_decltype/main.cpp
@@ -10,7 +10,9 @@ int main() {
   const int value = 10;
   auto x = value;
   auto y = &value;
+  // g++ 9.4:[error: aggregate ‘type_display<int> x_type’ has incomplete type and cannot be defined]
   type_display<decltype(x)> x_type;
+  // g++ 9.4: [error: aggregate ‘type_display<const int*> y_type’ has incomplete type and cannot be defined]
   type_display<decltype(y)> y_type;
 
   return 0;
