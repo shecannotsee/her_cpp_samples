349983a83b2d18e2551a915bec9151b1e287c3ab
shecannotsee <shecannotsee@163.com>
2023-09-21 18:31:47 +0800

    [feat] pass!code run good!

--- dynamic_programming _test/main.cpp
+++ dynamic_programming _test/main.cpp
@@ -5,7 +5,9 @@
 #include "t1_make_up_the_number_amount.h"
 
 int main(int argc,char** argv) {
-  t1_make_up_the_number_amount::main(argc,argv);
-
+  int range = std::atoi(argv[1]);
+  for (int i = 0; i <= range; ++i) {
+    t1_make_up_the_number_amount::main(i);
+  }
   return 0;
 }
--- dynamic_programming _test/t1_make_up_the_number_amount.h
+++ dynamic_programming _test/t1_make_up_the_number_amount.h
@@ -13,32 +13,32 @@
 namespace t1_make_up_the_number_amount {
 
 //
-void cal(int num, std::unordered_map<int,int>& num_paper) {
-  if (num == 0) {
-    return;
-  }
+int cal_model(int num, std::unordered_map<int,int>& num_paper) {
   if (num_paper.count(num) > 0) {
     // 已经计算过了
-    return;
   } else {
     int temp = 0;
     // 尚未计算
     /*zzz*/if (num > 11) {
-
+      int temp_11 = cal_model(num-11,num_paper) + num_paper[11];
+      int temp_05 = cal_model(num- 5,num_paper) + num_paper[5];
+      int temp_01 = cal_model(num- 1,num_paper) + num_paper[1];
+      if (temp_11<=temp_05 && temp_11<=temp_01) temp = temp_11;
+      if (temp_05<=temp_11 && temp_05<=temp_01) temp = temp_05;
+      if (temp_01<=temp_11 && temp_01<=temp_05) temp = temp_01;
     } else if (num >  5) {
-      cal(num-5,num_paper);
-      cal(num-1,num_paper);
-      int temp_1 = num_paper[1] + num_paper[num-1];
-      int temp_2 = num_paper[5] + num_paper[num-1];
-      (temp_1>temp_2) ? (temp = temp_2) : (temp = temp_1);
+      int temp_05 = cal_model(num- 5,num_paper) + num_paper[5];
+      int temp_01 = cal_model(num- 1,num_paper) + num_paper[1];
+      if (temp_05<=temp_01 ) temp = temp_05;
+      if (temp_01<=temp_05 ) temp = temp_01;
     } else if (num >  0) {
-      cal(num-1,num_paper);
-      temp = num_paper[1] + num_paper[num-1];
+      cal_model(num-1,num_paper);
+      temp = cal_model(num-1,num_paper) + num_paper[1];
     }
-
     num_paper.insert(std::make_pair(num,temp));
-    return;
   }
+
+  return num_paper[num];
 }
 
 int get_min_paper(int num) {
@@ -50,26 +50,16 @@ int get_min_paper(int num) {
   }
   /* file num_paper, Incremental */
   for (int i = 1; i <= num; i++) {
-    cal(i,num_paper);
+    int display = cal_model(i,num_paper);
+//    std::cout << i << "(min paper):" << display << std::endl;
   }
 
   return num_paper[num];
 }
 
-int main(int argc,char** argv) {
-  std::cout << "test start" << std::endl;
-  std::cout << "argc numbers:" << argc << std::endl;
-  int value = 0;
-  for (size_t i = 0; i < argc; i++) {
-    std::cout << argv[i] << (i==argc-1? "\n" : " ");
-    value = std::atoi(argv[1]);
-  }
-
-  std::cout << "===============\n";
-  std::cout << "value(11,5,1):[" << get_min_paper(value) << "]\n";
-
-  std::cout << "===============\ntest end" << std::endl;
-  return 0;
+void main(int value) {
+  auto ret = get_min_paper(value);
+  std::cout << "\033[32m" << value << "(11,5,1):[" << ret << "]\033[0m" << std::endl;
 }
 
 
