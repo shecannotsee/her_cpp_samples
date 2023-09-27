934aefeb9dd36dfd6cfb9da06c7adc69401ca7e8
shecannotsee <shecannotsee@163.com>
2023-09-27 13:40:07 +0800

    [feat] big little endian check

--- big_little_endian_switch_test/main.cpp
+++ big_little_endian_switch_test/main.cpp
@@ -9,13 +9,15 @@ int main() {
   std::cout << "\033[33m" << "test start" << "\033[0m" << std::endl;
 
   auto ret = endian_check();
-  (ret==endian::big)?([](){
+  (ret==endian::big)?
+  [](){
     std::cout << "big endian" << std::endl;
-  })():([](){
+  }() :
+  [](){
     std::cout << "little endian" << std::endl;
-  })();
-//  t1_big_little_check::main();
-//  t2_endian_switch::main();
+  }();
+  t1_big_little_check::main();
+  t2_endian_switch::main();
 
   std::cout << "\033[33m" << "test end" << "\033[0m" << std::endl;
   return 0;
