0a177ae46f3d2248e648a5c00251a6b8dce1579a
shecannotsee <shecannotsee@163.com>
2023-09-28 15:36:15 +0800

    [style] adjust spacing

--- my_bloom_filter_test/bit_map.h
+++ my_bloom_filter_test/bit_map.h
@@ -5,82 +5,80 @@
 #ifndef BIT_MAP_H
 #define BIT_MAP_H
 
-#define __SHE_TEST_
-#ifdef __SHE_TEST_
+#define SHE_TEST_
+#ifdef SHE_TEST_
 #include <iostream>
-#endif//!__SHE_TEST_
+#endif// SHE_TEST_
 
 #include <memory>
 
-
-class bit_map
-{
+class bit_map {
  public:
-  bit_map(int size=32){
+  explicit bit_map(int size=32) {
     m_readSize = size%sizeof(int)+1;
     m_ptr = new unsigned int[m_readSize];
     m_size = size;
     turnAllOff();
   };
 
-  ~bit_map(){
+  ~bit_map() {
     delete[] m_ptr;
     m_ptr = nullptr;
   };
 
  public:
   //将位图的第N位置1，返回1:操作成功，返回0:N越界
-  int turnOn(const int &bitNumber){
-    if(checkRange(bitNumber)){
+  int turnOn(const int &bitNumber) {
+    if(checkRange(bitNumber)) {
       int index = bitNumber/32;
       m_ptr[index] |= (1<<(bitNumber%32));
       return 1;
     }
-    else{
+    else {
       return 0;
     }
-  };
+  }
 
   //将位图的第N位置0，返回1:操作成功，返回0:N越界
-  int turnOff(const int &bitNumber){
+  int turnOff(const int &bitNumber) {
     if(checkRange(bitNumber)){
       int index = bitNumber/32;
       m_ptr[index] &= ~(1<<(bitNumber%32));
       return 1;
     }
-    else{
+    else {
       return 0;
     }
-  };
+  }
 
   //查看第N位的状态，返回1:为1，返回0:为0，返回-1:N越界
-  int find(const int &bitNumber){
-    if(checkRange(bitNumber)){
+  int find(const int &bitNumber) {
+    if(checkRange(bitNumber)) {
       int index = bitNumber/32;
       return (m_ptr[index]>>(bitNumber%32) ) &0x00000001;
     }
     else{
       return -1;
     }
-  };
+  }
 
   //将所有位置0
-  void turnAllOff(){
+  void turnAllOff() {
     for(int i=0;i<m_readSize;i++){
       m_ptr[i] = 0x00000000;
     }
-  };
+  }
 
   //将所有位置1
-  void turnAllOn(){
+  void turnAllOn() {
     for(int i=0;i<m_readSize;i++){
       m_ptr[i] = 0xffffffff;
     }
-  };
+  }
 
   //测试
-  void debug(){
-#ifdef __SHE_TEST_
+  void debug() {
+#ifdef SHE_TEST_
     std::cout<<"test in 33bit\n";
     std::cout<<m_size<<","<<m_readSize<<"\n";
     turnOn(31);
@@ -96,24 +94,23 @@ class bit_map
       std::cout<<find(i)<<",";
     }
     std::cout<<"\n"<<find(33)<<"\n";
-#endif//!__SHE_TEST_
-  };
+#endif// SHE_TEST_
+  }
 
  private:
-  int checkRange(const int &range){
+  int checkRange(const int &range) const {
     if(range>=0 && range<m_size){
       return 1;
     }
     else{
       return 0;
     }
-  };
+  }
 
   int m_size;//位图大小
   int m_readSize;//实际位图需要多少个int(需要多少个32位)
   unsigned int* m_ptr;//位图指针
 
 };
-
 
 #endif // BIT_MAP_H
--- my_bloom_filter_test/bloom_filter.h
+++ my_bloom_filter_test/bloom_filter.h
@@ -5,53 +5,51 @@
 #ifndef BLOOM_FILTER_H
 #define BLOOM_FILTER_H
 
-#define __SHE_TEST_
-#ifdef  __SHE_TEST_
+#define SHE_TEST_
+#ifdef  SHE_TEST_
 #include <iostream>
-#endif//!__SHE_TEST_
+#endif//!SHE_TEST_
 
 #include "bit_map.h"
 #include "hash.h"
 
-class bloom_filter
-{
+class bloom_filter {
  public:
-  bloom_filter(){
+  bloom_filter() {
     m_bitmap = new bit_map(1024);
-  };
+  }
 
-  ~bloom_filter(){
+  ~bloom_filter() {
     delete m_bitmap;
     m_bitmap = nullptr;
-  };
+  }
 
-  void add(const char *str){
+  void add(const char *str) {
     m_bitmap->turnOn(HashBKDR<char>(str)%1024);
     m_bitmap->turnOn(HashAP<char>(str)%1024);
     m_bitmap->turnOn(HashDEK<char>(str)%1024);
-  };
+  }
 
-  int find(const char *str){
+  int find(const char *str) {
     if(m_bitmap->find(HashBKDR<char>(str)%1024)&&
        m_bitmap->find(HashAP<char>(str)%1024)&&
        m_bitmap->find(HashDEK<char>(str)%1024)
-      ){
+      ) {
       return 1;
     }
-    else{
+    else {
       return 0;
     }
-  };
+  }
 
-  void debug(){
-#ifdef __SHE_TEST_
-    std::cout<<"Bloom Fliter Debug.\n";
-#endif//!__SHE_TEST_
-  };
+  static void debug() {
+#ifdef SHE_TEST_
+    std::cout<<"Bloom Filter Debug.\n";
+#endif// SHE_TEST_
+  }
 
  private:
   bit_map *m_bitmap;
-
 };
 
 #endif // BLOOM_FILTER_H
