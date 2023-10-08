904af7707ea9c3cd9dd3749efda357c19f66f484
shecannotsee <shecannotsee@163.com>
2023-10-08 16:59:45 +0800

    [feat] remove c++ to qml var

--- qml_demo/t4_more_qml_windows_switch_test.h
+++ qml_demo/t4_more_qml_windows_switch_test.h
@@ -12,7 +12,6 @@
 namespace t4_more_qml_windows_switch_test {
 
 int main(int argc, char* argv[]) {
-
   QGuiApplication app(argc, argv);
 
   QQmlApplicationEngine engine;
@@ -22,8 +21,6 @@ int main(int argc, char* argv[]) {
 
   // 将登录处理器对象注册到 QML 上下文中
   engine.rootContext()->setContextProperty("loginHandler", &loginHandler);
-  engine.rootContext()->setContextProperty("DISPLAY",true);
-
 
   // 加载 QML 文件
   engine.load(QUrl(QStringLiteral("../qml_resources/t4_login.qml")));
