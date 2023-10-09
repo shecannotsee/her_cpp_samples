c4cd3e37f3d9a04e1d2676cb3f9294c8a948bd01
shecannotsee <shecannotsee@163.com>
2023-10-09 23:42:31 +0800

    [style] format

--- pgsql_client_test/t1_connect_test.h
+++ pgsql_client_test/t1_connect_test.h
@@ -127,9 +127,7 @@ void demo1() {
  * a table defined as in the comment above.  We split it out because the
  * main() function uses it twice.
  */
-static void
-show_binary_results(PGresult *res)
-{
+static void show_binary_results(PGresult *res) {
   int         i,
     j;
   int         i_fnum,
@@ -183,6 +181,14 @@ show_binary_results(PGresult *res)
 }
 
 void demo2() {
+  /*
+CREATE SCHEMA testlibpq3;
+SET search_path = testlibpq3;
+SET standard_conforming_strings = ON;
+CREATE TABLE test1 (i int4, t text, b bytea);
+INSERT INTO test1 values (1, 'joe''s place', '\000\001\002\003\004');
+INSERT INTO test1 values (2, 'ho there', '\004\003\002\001\000');
+   */
   const char *conninfo;
   PGconn     *conn;
   PGresult   *res;
