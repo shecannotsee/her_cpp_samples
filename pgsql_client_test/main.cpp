7ba2c80f49b8aef254ad86781ead566440f7429c
shecannotsee <shecannotsee@163.com>
2023-10-09 21:45:37 +0800

    [feat] project add

---
+++ pgsql_client_test/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by shecannotsee on 23-10-9.
+//
+#include <iostream>
+
+
+int main(int argc, char *argv[]) {
+  std::cout << "\033[33m" << "test start" << "\033[0m" << std::endl;
+
+
+  std::cout << "\033[33m" << "test end" << "\033[0m" << std::endl;
+  return 0;
+}
