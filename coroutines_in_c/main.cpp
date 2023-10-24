afcca8f4d8c5f947ebe17628d4d8c0233c28c743
shecannotsee <shecannotsee@163.com>
2023-10-24 14:43:24 +0800

    [feat] add framework

---
+++ coroutines_in_c/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by shecannotsee on 23-10-24.
+//
+#include <iostream>
+#include "t1_get_test.h"
+
+int main() {
+  std::cout << "start" << std::endl;
+  t1_get_test::main();
+
+  std::cout << "done" << std::endl;
+  return 0;
+}
---
+++ coroutines_in_c/t1_get_test.h
@@ -0,0 +1,18 @@
+//
+// Created by shecannotsee on 23-10-24.
+//
+
+#ifndef COROUTINES_IN_C_T1_GET_TEST_H
+#define COROUTINES_IN_C_T1_GET_TEST_H
+
+namespace t1_get_test {
+
+void main() {
+
+
+}
+
+
+}// namespace t1_get_test
+
+#endif //COROUTINES_IN_C_T1_GET_TEST_H
