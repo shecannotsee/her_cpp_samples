03be56b57ee24800059b49a6e113823bf40c8d6d
shecannotsee <shecannotsee@163.com>
2023-11-07 12:01:58 +0800

    [feat] 完全处理了流和大小端的转换问题，包括16-32位

--- big_little_endian_switch_test/t3_stream_and_endian.h
+++ big_little_endian_switch_test/t3_stream_and_endian.h
@@ -12,14 +12,47 @@
 namespace t3_stream_and_endian {
 
 void main() {
-  std::uint8_t stream[2]={0xF0,0x0F};// 从左往右也就是0xF00F的十进制表示是61455,0x0FF0是4080
-  std::uint16_t data = 0xF00F;
-  std::cout << data << std::endl;
-  std::uint16_t data_big = htons(data);
-  std::cout << data_big << std::endl;
+  /* 16位的测试与处理 */ {
+    std::cout << "\033[32m" << "16 bits test" << "\033[0m" << std::endl;
+    std::uint8_t stream[2] = {0xF0, 0x0F};// 从左往右也就是0xF00F的十进制表示是61455,0x0FF0是4080
+    std::uint16_t data = 0xF00F;
+    std::cout << data << std::endl;
+    std::uint16_t data_big = htons(data);
+    std::cout << data_big << std::endl;
 
-  std::uint16_t b = (static_cast<std::uint8_t>(stream[0]) << 8) | static_cast<std::uint8_t>(stream[1]);
-  std::cout << b << std::endl;
+    // 小端处理法
+    std::uint16_t b_little = (static_cast<std::uint8_t>(stream[0]) << 8) |
+                             (static_cast<std::uint8_t>(stream[1]) << 0);
+    std::cout << b_little << std::endl;
+    // 大端处理法
+    std::uint16_t b_big = (static_cast<std::uint8_t>(stream[1]) << 8) |
+                          (static_cast<std::uint8_t>(stream[0]) << 0);
+    std::cout << b_big << std::endl;
+  }
+
+  /* 32位的测试与处理 */ {
+    std::cout << "\033[32m" << "32 bits test" << "\033[0m" << std::endl;
+    std::uint8_t stream[4] = {0xF0, 0x0F, 0x00 ,0x00};// 从左往右也就是0xF00F0000的十进制表示是4027514880₁₀,0x00000FF0是4080
+    std::uint32_t data = 0xF00F0000;
+    std::cout << data << std::endl;
+    std::uint32_t data_big = htonl(data);
+    std::cout << data_big << std::endl;
+
+    // 小端处理法
+    std::uint32_t b_little = (static_cast<std::uint8_t>(stream[0]) << 24) |
+                             (static_cast<std::uint8_t>(stream[1]) << 16) |
+                             (static_cast<std::uint8_t>(stream[2]) <<  8) |
+                             (static_cast<std::uint8_t>(stream[3]) <<  0);
+    std::cout << b_little << std::endl;
+    // 大端处理法
+    std::uint32_t b_big = (static_cast<std::uint8_t>(stream[3]) << 24) |
+                          (static_cast<std::uint8_t>(stream[2]) << 16) |
+                          (static_cast<std::uint8_t>(stream[1]) <<  8) |
+                          (static_cast<std::uint8_t>(stream[0]) <<  0);
+    std::cout << b_big << std::endl;
+  }
+
+
 }
 
 }
