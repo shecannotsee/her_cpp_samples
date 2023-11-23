fc3363a8424a0ad8410019fc56c9c469c1e4a159
shecannotsee <shecannotsee@163.com>
2023-11-23 18:28:16 +0800

    [feat] Add temporary file processing strategy

--- openssl1.1.1s_test/main.cpp
+++ openssl1.1.1s_test/main.cpp
@@ -5,12 +5,14 @@
 // #include "openssl_aes.h"
 #include "t1_compress_data.h"
 #include "t2_decompress_data.h"
+#include "t3_compress_data.h"
 
 int main() {
   std::cout << "start.\n";
 
   t1_compress_data::main();
   t2_decompress_data::main();
+  t3_compress_data::main();
 
   std::cout << "Done.\n";
   return 0;
