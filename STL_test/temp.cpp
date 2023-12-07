bdd192ce521b11ebf5c5b24c1671d891a681e1b7
shecannotsee <shecannotsee@163.com>
2023-12-07 17:54:43 +0800

    [feat] 1.macro testing

--- STL_test/temp.cpp
+++ STL_test/temp.cpp
@@ -9,7 +9,11 @@ int main() {
   char s[] = {'1','2','3','0','0','4','5'};
   std::cout<<strlen(s)<<"\n";
   printf("[%s]\n",s);
-
+  std::cout << "#[" << __TIME__<< "]\n";
+  std::cout << "#[" << __TIMESTAMP__ << "]\n";
+  std::cout << "#[" << __func__ << "]\n";
+  std::cout << "#[" << __FUNCTION__ << "]\n";
+  std::cout << "#[" << __LINE__ << "]\n";
 
   std::cout<<s;
   std::cout<<"|||\n";
