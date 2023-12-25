c8e00a02928ada5195d0bab3ccbd215915439ecc
shecannotsee <shecannotsee@163.com>
2023-12-25 14:41:27 +0800

    [feat] 1.Add exit and exception test

---
+++ googletest_test/t2_gmock_easy.cpp
@@ -0,0 +1,35 @@
+//
+// Created by shecannotsee on 23-12-25.
+//
+
+#ifndef T2_GMOCK_EASY_H
+#define T2_GMOCK_EASY_H
+
+#include "global_test_set.h"
+
+namespace t2_gmock_easy {
+
+class function {
+public:
+  virtual int get_number_8() const = 0;
+  virtual void do_this(int n) = 0;
+};
+
+class mock_function : public function {
+public:
+  MOCK_METHOD(int, get_number_8, (), (const, override));
+  MOCK_METHOD(void, do_this, (int n), (override));
+};
+
+}// namespace t2_gmock_easy
+
+TEST(t2_gmock_easy, function_test) {
+  t2_gmock_easy::mock_function temp;/* set action */ {
+    EXPECT_CALL(temp, get_number_8())
+      .WillOnce(testing::Return(8));
+  };
+
+  EXPECT_EQ(8,temp.get_number_8());
+};
+
+#endif //T2_GMOCK_EASY_H
---
+++ googletest_test/t3_gmock_test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by shecannotsee on 23-12-25.
+//
+
+#ifndef T3_GMOCK_TEST_H
+#define T3_GMOCK_TEST_H
+
+#include "global_test_set.h"
+
+
+#endif //T3_GMOCK_TEST_H
