64913429fff0cf98c3268aab2a9a5d5a87bc0f3a
shecannotsee <shecannotsee@163.com>
2024-01-30 12:03:07 +0800

    [feat] base code complete

--- Template_Metaprogramming_test/she_type_traits.h
+++ Template_Metaprogramming_test/she_type_traits.h
@@ -10,7 +10,7 @@
 
 namespace she {
 
-template<typename type_, type_ value_>
+template <typename type_, type_ value_>
 struct constant_base {
   static constexpr type_ value = value_;
   static std::string get_value() {
@@ -21,17 +21,32 @@ struct constant_base {
     }
   }
   using value_type = type_;
-  using type = constant_base;
+  using type       = constant_base<type_, value_>;
+  constexpr operator value_type() const noexcept {
+    return value;
+  }
+  constexpr value_type operator()() const noexcept {
+    return value;
+  }
 };
 
-using true_type = constant_base<bool, true>;
+using true_type  = constant_base<bool, true>;
 using false_type = constant_base<bool, false>;
 
 // 这是一个条件模板，根据一个布尔值的真假选择两个不同的类型。在这里，根据第一个参数（布尔值）的真假，选择第二个类型或者第三个类型。
-template <bool, typename true_, typename false_>
-struct group;
+template <bool c, typename true_, typename false_>
+struct group {
+  using type = true_;
+};
+template <typename true_, typename false_>
+struct group<false, true_, false_> {
+  typedef false_ type;
+};
 
-// or //
+// not
+template <typename p>
+struct _not_ : public constant_base<bool, !bool(p::value)> {};
+// or
 template <typename...>
 struct _or_;
 template <>
@@ -40,39 +55,19 @@ template <typename c1>
 struct _or_<c1> : public c1 {};
 template <typename c1, typename c2>
 struct _or_<c1, c2> : public group<c1::value, c1, c2>::type {};
-
-
-// // 当只有一个模板参数 c1 时，_or_ 继承自 c1。这表示如果 c1::value 为真，整个 _or_ 类型就被设为真，否则为假。
-// template <typename c1>
-// struct _or_<c1> : public c1 {};
-// // 这是 _or_ 的另一个特化，用于处理两个模板参数 c1 和 c2 的情况。根据 c1::value 的真假，选择继承自 c1 或者 c2。
-// template <typename c1, typename c2>
-// struct _or_<c1, c2> : public group<c1::value, c1, c2>::type {};
-// // 这是 _or_ 的通用特化，处理三个或更多的模板参数。根据 c1::value 的真假，选择继承自 c1 或者递归地继承自 _or_<c2, c3,
-// // cn...>
-// template <typename c1, typename c2, typename c3, typename... cn>
-// struct _or_<c1, c2, c3, cn...> : public group<c1::value, c1, _or_<c2, c3, cn...>>::type {};
-//
-// /// and
-// template <typename...>
-// struct _and_;
-//
-// template <>
-// struct _and_<> : public true_type {};
-//
-// template <typename c1>  // condition 1
-// struct _and_<c1> : public c1 {};
-//
-// template <typename c1, typename c2>
-// struct _and_<c1, c2> : public group<c1::value, c2, c1>::type {};
-//
-// template <typename c1, typename c2, typename c3, typename... cn>
-// struct _and_<c1, c2, c3, cn...> : public group<c1::value, _and_<c2, c3, cn...>, c1>::type {};
-//
-// /// not
-// template <typename p>
-// struct _not_ : public bool_constant<!bool(p::value)> {};
-
+template <typename c1, typename c2, typename c3, typename... cn>
+struct _or_<c1, c2, c3, cn...> : public group<c1::value, c1, _or_<c2, c3, cn...>>::type {};
+// and
+template <typename...>
+struct _and_;
+template <>
+struct _and_<> : public true_type {};
+template <typename c1>
+struct _and_<c1> : public c1 {};
+template <typename c1, typename c2>
+struct _and_<c1, c2> : public group<c1::value, c2, c1>::type {};
+template <typename c1, typename c2, typename c3, typename... cn>
+struct _and_<c1, c2, c3, cn...> : public group<c1::value, _and_<c2, c3, cn...>, c1>::type {};
 }
 
 #endif //SHE_TYPE_TRAITS_H
--- Template_Metaprogramming_test/t4_type_traits.h
+++ Template_Metaprogramming_test/t4_type_traits.h
@@ -5,7 +5,7 @@
 #ifndef T4_TYPE_TRAITS_H
 #define T4_TYPE_TRAITS_H
 
-#define PRINT(module, str) std::cout<<"["<<(module)<<"]:"<<(str)<<std::endl
+#define PRINT(module, str) std::cout << "[" << (module) << "]:" << (str) << std::endl
 
 #include "she_type_traits.h"
 
@@ -13,18 +13,74 @@ namespace t4_type_traits {
 
 void run() {
   using namespace she;
-  PRINT("true_type",
-    true_type::get_value());
-  PRINT("false_type",
-    false_type::get_value());
-  PRINT("_or_<>",
-    _or_<>::get_value());
-  PRINT("_or_<false_type>",
-    _or_<false_type>::get_value());
-  PRINT("_or_<true_type>",
-    _or_<true_type>::get_value());
+
+  PRINT("true_type", true_type::get_value());
+  PRINT("false_type", false_type::get_value());
+  PRINT("_or_<>", _or_<>::get_value());
+  PRINT("_or_<false_type>", _or_<false_type>::get_value());
+  PRINT("_or_<true_type>", _or_<true_type>::get_value());
+  /* !1 */ {
+    using t = _not_<true_type>;
+    PRINT("_not_<true_type>", t::get_value());
+  }
+  /* !0 */ {
+    using t = _not_<false_type>;
+    PRINT("_not_<false_type>", t::get_value());
+  }
+  /* 1|1 */ {
+    using t = _or_<true_type, true_type>;
+    PRINT("_or_<true_type, true_type>", t::get_value());
+  }
+  /* 1|0 */ {
+    using t = _or_<true_type, false_type>;
+    PRINT("_or_<true_type, false_type>", t::get_value());
+  }
+  /* 0|1 */ {
+    using t = _or_<false_type, true_type>;
+    PRINT("_or_<false_type, true_type>", t::get_value());
+  }
+  /* 0|0 */ {
+    using t = _or_<false_type, false_type>;
+    PRINT("_or_<false_type, false_type>", t::get_value());
+  }
+  /* 1|0|1|1 */ {
+    using t = _or_<true_type, false_type, true_type, true_type>;
+    PRINT("_or_<true_type, false_type, true_type, true_type>", t::get_value());
+  }
+  /* 0|0|0|0 */ {
+    using t = _or_<false_type, false_type, false_type, false_type>;
+    PRINT("_or_<false_type, false_type, false_type, false_type>", t::get_value());
+  }
+  /* 1&1 */ {
+    using t = _and_<true_type,true_type>;
+    PRINT("_and_<true_type,true_type>", t::get_value());
+  }
+  /* 1&0 */ {
+    using t = _and_<true_type,false_type>;
+    PRINT("_and_<true_type,false_type>", t::get_value());
+  }
+  /* 0&1 */ {
+    using t = _and_<false_type,true_type>;
+    PRINT("_and_<false_type,true_type>", t::get_value());
+  }
+  /* 0&0 */ {
+    using t = _and_<false_type,false_type>;
+    PRINT("_and_<false_type,false_type>", t::get_value());
+  }
+  /* 1 & 1 & 1 & 1 */ {
+    using t = _and_<true_type, true_type, true_type, true_type>;
+    PRINT("_and_<true_type, true_type, true_type, true_type>", t::get_value());
+  }
+  /* 1 & 0 & 1 & 1 */ {
+    using t = _and_<true_type, false_type, true_type, true_type>;
+    PRINT("_and_<true_type, false_type, true_type, true_type>", t::get_value());
+  }
+  /* 0 & 0 & 0 & 0 */ {
+    using t = _and_<false_type, false_type, false_type, false_type>;
+    PRINT("_and_<false_type, false_type, false_type, false_type>", t::get_value());
+  }
 }
 
-}
+}  // namespace t4_type_traits
 
-#endif //T4_TYPE_TRAITS_H
+#endif  // T4_TYPE_TRAITS_H
