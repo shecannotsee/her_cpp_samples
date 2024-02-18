c2dad022edc1eba419150b603549f9bc88ca7afd
shecannotsee <shecannotsee@163.com>
2024-02-18 18:03:36 +0800

    [feat] Narrow example

--- gsl-4-0-0_test/t4_narrow.h
+++ gsl-4-0-0_test/t4_narrow.h
@@ -10,6 +10,30 @@
 namespace t4_narrow {
 
 void run() noexcept {
+  std::cout << YELLOW_COLOR << "[t4_narrow::run] >>> start" << RESET_COLOR << std::endl;
+  gsl::final_action<std::function<void()>> _(
+      []() { std::cout << GREEN_COLOR << "[t4_narrow::run] >>> done" << RESET_COLOR << "\n\n"; });
+
+  // error process
+  try {
+    // 128 > (-127~127)
+    constexpr int sourceValue  = 128;
+    constexpr char targetValue = gsl::narrow<char>(sourceValue);
+    // debug
+    std::cout << "Source value: " << sourceValue << std::endl;
+    std::cout << "Target value (char): " << static_cast<int>(targetValue) << std::endl;
+  } catch (const gsl::narrowing_error& e) {
+    // error debug
+    std::cout << RED_COLOR << "int(128) to char(-127~127) error" << std::endl;
+    std::cout << e.what() << std::endl << RESET_COLOR;
+  }
+
+  // Right process
+  constexpr int sourceValue  = 127;
+  constexpr char targetValue = gsl::narrow<char>(sourceValue);
+  // debug
+  std::cout << "Source value (int): " << sourceValue << std::endl;
+  std::cout << "Target value (char): " << static_cast<int>(targetValue) << std::endl;
 }
 
 }  // namespace t4_narrow
