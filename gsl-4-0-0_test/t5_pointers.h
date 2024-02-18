090244e5c0e8bf432131cfa1ab6d373e780124cf
shecannotsee <shecannotsee@163.com>
2024-02-18 18:11:44 +0800

    [feat] Add example

--- gsl-4-0-0_test/t5_pointers.h
+++ gsl-4-0-0_test/t5_pointers.h
@@ -9,22 +9,38 @@
 
 namespace t5_pointers {
 
-gsl::owner<int*> allocateResource() {
+// 使用 gsl::owner 作为函数返回类型
+gsl::owner<int*> createInt() {
   return new int(42);
 }
 
-void useResource(gsl::owner<int*> resource) {
-  // 使用资源
-  // ...
-
-  // 资源将在函数结束时释放，调用方不再拥有它
-  delete resource;
+// 使用 gsl::not_null 作为函数参数类型
+void printValue(gsl::not_null<int*> ptr) {
+  std::cout << "Value: " << *ptr << std::endl;
 }
 
-
 void run() noexcept {
-  auto p = allocateResource();
-  useResource(p);
+  std::cout << YELLOW_COLOR << "[t5_pointers::run] >>> start" << RESET_COLOR << std::endl;
+  gsl::final_action<std::function<void()>> _(
+      []() { std::cout << GREEN_COLOR << "[t5_pointers::run] >>> done" << RESET_COLOR << "\n\n"; });
+
+  // 使用 gsl::owner 作为指针类型
+  gsl::owner<int*> ptr = createInt();
+
+  // 使用 gsl::not_null 作为指针类型
+  gsl::not_null<int*> notNullPtr = gsl::make_not_null(ptr);
+
+  // 使用 gsl::strict_not_null 作为指针类型
+  gsl::strict_not_null<int*> strictNotNullPtr = gsl::make_strict_not_null(ptr);
+
+  // 使用 not_null 指针调用函数
+  printValue(notNullPtr);
+
+  // 使用 strict_not_null 指针调用函数
+  printValue(strictNotNullPtr);
+
+  // 释放指针内存
+  delete ptr;
 }
 
 }  // namespace t5_pointers
