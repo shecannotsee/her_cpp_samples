4529efb1432616b874de8beb1771a4f2a4045d0d
shecannotsee <shecannotsee@163.com>
2024-02-18 18:24:38 +0800

    [feat] Add example

--- gsl-4-0-0_test/t6_span.h
+++ gsl-4-0-0_test/t6_span.h
@@ -10,6 +10,29 @@
 namespace t6_span {
 
 void run() noexcept {
+  std::cout << YELLOW_COLOR << "[t6_span::run] >>> start" << RESET_COLOR << std::endl;
+  gsl::final_action<std::function<void()>> _(
+      []() { std::cout << GREEN_COLOR << "[t6_span::run] >>> done" << RESET_COLOR << "\n\n"; });
+
+  int arr[] = {1, 2, 3, 4, 5};
+  // &arr
+  const gsl::span<int> my_span(arr, 5);
+  // print
+  std::cout << "Span size: " << my_span.size() << std::endl;
+  std::cout << "Span elements:";
+  for (const int num : my_span) {
+    std::cout << " " << num;
+  }
+  std::cout << std::endl;
+
+  // modify
+  arr[2] = 100;
+  // check
+  std::cout << "Modified span elements:";
+  for (const int num : my_span) {
+    std::cout << " " << num;
+  }
+  std::cout << std::endl;
 }
 
 }  // namespace t6_span
