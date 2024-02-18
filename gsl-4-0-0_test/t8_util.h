1c8e594d90d5df7fe816d28671b3e7c6640955f5
shecannotsee <shecannotsee@163.com>
2024-02-18 16:25:03 +0800

    [feat] Add method that how to use final_action

--- gsl-4-0-0_test/t8_util.h
+++ gsl-4-0-0_test/t8_util.h
@@ -5,11 +5,18 @@
 #ifndef T8_UTIL_H
 #define T8_UTIL_H
 
+#include <functional>
+
 #include "global_test_set.h"
+#include "gsl/gsl"
 
 namespace t8_util {
 
 void run() noexcept {
+  std::cout << PURPLE_COLOR << "t8_util start." << std::endl;
+  gsl::final_action<std::function<void()>> p([]() { std::cout << "t8_util::run: dtor" << RESET_COLOR << std::endl; });
+  std::cout << __LINE__ << std::endl;
+  std::cout << "t8_util done." << std::endl;
 }
 
 }  // namespace t8_util
