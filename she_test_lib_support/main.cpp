6e62a7b806d48d5d7c33bb307712e3bae8c60d32
shecannotsee <shecannotsee@163.com>
2024-05-10 14:30:31 +0800

    [test] 1.Add check method

--- she_test_lib_support/main.cpp
+++ she_test_lib_support/main.cpp
@@ -8,19 +8,37 @@
 int main() {
   {
     const auto source_code = a::b();
+    unsigned int flag = 0;
 
     std::cout << "Process source_code ......\n";
     std::cout << source_code << std::endl;
     std::cout << remove_spaces_and_new_lines(source_code) << std::endl;
-    std::cout << adler32(remove_spaces_and_new_lines(source_code)) << std::endl;
+    std::cout << [&flag, &source_code]() {
+      flag = adler32(remove_spaces_and_new_lines(source_code));
+      return flag;
+    }() << std::endl;
+    if (flag == 1008534972) {
+      std::cout << "The source code has not been modified\n";
+    } else {
+      std::cout << "The source code has been modified\n";
+    }
   }
   {
     const auto source_code = a::b1();
+    unsigned int flag = 0;
 
     std::cout << "Process source_code ......\n";
     std::cout << source_code << std::endl;
     std::cout << remove_spaces_and_new_lines(source_code) << std::endl;
-    std::cout << adler32(remove_spaces_and_new_lines(source_code)) << std::endl;
+    std::cout << [&flag, &source_code]() {
+      flag = adler32(remove_spaces_and_new_lines(source_code));
+      return flag;
+    }() << std::endl;
+    if (flag == 816324916) {
+      std::cout << "The source code has not been modified\n";
+    } else {
+      std::cout << "The source code has been modified\n";
+    }
   }
 
   return 0;
