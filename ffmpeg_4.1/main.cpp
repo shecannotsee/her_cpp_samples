054862b694cd91008d94f328fd9696c9b640bf36
shecannotsee <shecannotsee@163.com>
2024-05-28 12:19:43 +0800

    [feat] 1.Add gop partition

--- ffmpeg_4.1/main.cpp
+++ ffmpeg_4.1/main.cpp
@@ -4,10 +4,12 @@
 
 #include <iostream>
 
+#include "cut_frame_to_h264.h"
 #include "rtsp_to_h264.h"
 
 int main() {
   std::cout << "ffmpeg_4.1 test start" << std::endl;
   rtsp_to_h264::run();
+  // cut_frame_to_h264::run();
   return 0;
 }
