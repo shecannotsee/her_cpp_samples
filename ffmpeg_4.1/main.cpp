c7962e48871625f57facbe1978c18be7a8f6c2b5
shecannotsee <shecannotsee@163.com>
2024-05-29 14:37:44 +0800

    [feat] 1.Add rtsp to jpeg code

--- ffmpeg_4.1/main.cpp
+++ ffmpeg_4.1/main.cpp
@@ -6,10 +6,12 @@
 
 #include "cut_frame_to_h264.h"
 #include "rtsp_to_h264.h"
+#include "rtsp_to_jpeg.h"
 
 int main() {
   std::cout << "ffmpeg_4.1 test start" << std::endl;
-  rtsp_to_h264::run();
+  // rtsp_to_h264::run();
   // cut_frame_to_h264::run();
+  rtsp_to_jpeg::run();
   return 0;
 }
