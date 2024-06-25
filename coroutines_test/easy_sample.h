d41a080fe675b407835da551084e3759028392ee
shecannotsee <shecannotsee@163.com>
2024-06-25 12:28:27 +0800

    [feat] 1.sample coroutines

---
+++ coroutines_test/easy_sample.h
@@ -0,0 +1,79 @@
+#ifndef EASY_SAMPLE_H
+#define EASY_SAMPLE_H
+
+#include <coroutine>
+
+namespace easy_sample {
+
+struct simple_awaiter {
+  constexpr bool await_ready() const noexcept {
+    printf("----simple_awaiter::await_ready\n");
+    return true;
+  }
+
+  constexpr void await_suspend(std::coroutine_handle<>) const noexcept {
+    printf("----simple_awaiter::await_suspend\n");
+  }
+
+  constexpr void await_resume() const noexcept {
+    printf("----simple_awaiter::await_resume\n");
+  }
+};
+
+// The caller-level type
+struct frame {
+  // 固定名称
+  struct promise_type {
+    // 固定名称:step1
+    static frame get_return_object() {
+      printf("--get_return_object\n");
+      return {};
+    }
+    // 固定名称:step2
+    static simple_awaiter initial_suspend() {
+      printf("--initial_suspend\n");
+      return {};
+    }
+    // function start......
+    // ......
+
+    // 固定名称,在co_return后调用step1
+    static void return_void() {
+      printf("--return_void\n");
+    }
+
+    // 固定名称,在co_return后调用step2
+    static simple_awaiter final_suspend() noexcept {
+      printf("--final_suspend\n");
+      return {};
+    }
+    // 固定名称
+    static void unhandled_exception() {
+      printf("--unhandled_exception\n");
+    }
+  };
+
+  frame() {
+    printf("[frame]\n");
+  }
+
+  ~frame() {
+    printf("[~frame]\n");
+  }
+};
+
+// 协程函数
+frame my_coroutine() {
+  printf("start co_return\n");
+  co_return;  // make it a coroutine
+}
+
+inline void run() {
+  printf("run start\n");
+  frame x = my_coroutine();
+  printf("run done\n");
+}
+
+}  // namespace easy_sample
+
+#endif  // EASY_SAMPLE_H
---
+++ coroutines_test/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "easy_sample.h"
+
+int main() {
+  std::cout << "coroutines_test start.\n";
+  easy_sample::run();
+
+  std::cout << "coroutines_test done.\n";
+  return 0;
+}
