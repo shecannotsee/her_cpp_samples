1db0091d4db8ded5cdb3f741893799e02615a244
shecannotsee <shecannotsee@163.com>
2024-06-28 15:30:16 +0800

    [feat] 1.chmod +x build_dependencies.sh 2.modify method name

--- pybind11_test/test_call.py
+++ pybind11_test/test_call.py
@@ -1,13 +1,13 @@
 import sys
 sys.path.insert(0, './cmake-build-debug')
-import api_1
-import api_2
+import double_vector_1D
+import double_vector_2D
 
 A = [1.,2.,3.,4.]
-B = api_1.modify(A)
+B = double_vector_1D.modify(A)
 print(B)
 
 print("###################")
 C = [[1,2,3,4],[5,6]]
-D = api_2.modify(C)
+D = double_vector_2D.modify(C)
 print(D)
