e357fae18360abe347d2b6c8ae799412263199bc
shecannotsee <shecannotsee@163.com>
2024-10-08 18:30:33 +0800

    [feat] 1.remove to https://github.com/shecannotsee/sheer_third_party/tree/main/sample/sqlite-3.45.0

--- sqlite3_test/createDB.bash
+++
@@ -1,2 +0,0 @@
-sqlite3 test.db
-sqlite3 test.db < createTable.sql
--- sqlite3_test/createTableStandard.sql
+++
@@ -1,6 +0,0 @@
-/*注释*/
-create TABLE  role(
-    role_id         int     NOT NULL,
-    role_describe   text    NOT NULL,
-    PRIMARY KEY (role_id)
-);
